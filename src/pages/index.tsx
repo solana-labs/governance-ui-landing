e41608b762b915e0e1c157775f1705c5f8aac08b
Niranjan Ramadas <mail@niranjan.me>
2022-11-05 19:45:56 +0000

    feat: add a redirect to the discover page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,52 @@
-import usePageViewTracking from '@/hooks/usePageViewTracking';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+// import usePageViewTracking from '@/hooks/usePageViewTracking';
 
-import DAOStructures from '@/components/DAOStructures';
-import Footer from '@/components/Footer';
-import Introduction from '@/components/Introduction';
-import ManagingDAOs from '@/components/ManagingDAOs';
-import Metrics from '@/components/Metrics';
-import NavBar from '@/components/NavBar';
-import RealmsCommunity from '@/components/RealmsCommunity';
-import SolanaStandard from '@/components/SolanaStandard';
-import TrackViewportEnter from '@/components/TrackViewportEnter';
+// import DAOStructures from '@/components/DAOStructures';
+// import Footer from '@/components/Footer';
+// import Introduction from '@/components/Introduction';
+// import ManagingDAOs from '@/components/ManagingDAOs';
+// import Metrics from '@/components/Metrics';
+// import NavBar from '@/components/NavBar';
+// import RealmsCommunity from '@/components/RealmsCommunity';
+// import SolanaStandard from '@/components/SolanaStandard';
+// import TrackViewportEnter from '@/components/TrackViewportEnter';
 
 const Index = () => {
-  usePageViewTracking('index');
+  // usePageViewTracking('index');
+  const router = useRouter();
 
-  return (
-    <main>
-      <NavBar />
-      <TrackViewportEnter name='introduction'>
-        <Introduction />
-      </TrackViewportEnter>
-      <TrackViewportEnter name='metrics'>
-        <Metrics />
-      </TrackViewportEnter>
-      <TrackViewportEnter name='managing_daos'>
-        <ManagingDAOs />
-      </TrackViewportEnter>
-      <TrackViewportEnter name='dao_structures'>
-        <DAOStructures trackingContext='index_dao_structures' />
-      </TrackViewportEnter>
-      <TrackViewportEnter name='realms_community'>
-        <RealmsCommunity />
-      </TrackViewportEnter>
-      <TrackViewportEnter name='solana_standard'>
-        <SolanaStandard />
-      </TrackViewportEnter>
-      <TrackViewportEnter name='footer'>
-        <Footer className='mt-12 mb-36 w-full px-[20px] sm:my-24 sm:gap-x-40 md:px-24 lg:px-24' />
-      </TrackViewportEnter>
-    </main>
-  );
+  useEffect(() => {
+    router.push('https://app.realms.today/discover');
+  }, [router]);
+
+  return null;
+  // return (
+  //   <main>
+  //     <NavBar />
+  //     <TrackViewportEnter name='introduction'>
+  //       <Introduction />
+  //     </TrackViewportEnter>
+  //     <TrackViewportEnter name='metrics'>
+  //       <Metrics />
+  //     </TrackViewportEnter>
+  //     <TrackViewportEnter name='managing_daos'>
+  //       <ManagingDAOs />
+  //     </TrackViewportEnter>
+  //     <TrackViewportEnter name='dao_structures'>
+  //       <DAOStructures trackingContext='index_dao_structures' />
+  //     </TrackViewportEnter>
+  //     <TrackViewportEnter name='realms_community'>
+  //       <RealmsCommunity />
+  //     </TrackViewportEnter>
+  //     <TrackViewportEnter name='solana_standard'>
+  //       <SolanaStandard />
+  //     </TrackViewportEnter>
+  //     <TrackViewportEnter name='footer'>
+  //       <Footer className='mt-12 mb-36 w-full px-[20px] sm:my-24 sm:gap-x-40 md:px-24 lg:px-24' />
+  //     </TrackViewportEnter>
+  //   </main>
+  // );
 };
 
 export default Index;
