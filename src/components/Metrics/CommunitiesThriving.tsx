8331ba6e5acac3138914f351a93dfcf126ac2e97
agrippa kellum <ask252@cornell.edu>
2023-06-26 16:22:06 -0400

    chore: add helium as community

--- src/components/Metrics/CommunitiesThriving.tsx
+++ src/components/Metrics/CommunitiesThriving.tsx
@@ -6,12 +6,12 @@ import Content from './Content';
 import Section from './Section';
 
 const DAOS = [
-  { src: '/assets/dao/blue-grape.png', title: 'GRAPE' },
-  { src: '/assets/dao/blue-uxd.png', title: 'UXDProtocol' },
   { src: '/assets/dao/blue-mango.png', title: 'Mango DAO' },
+  { src: '/assets/dao/blue-grape.png', title: 'GRAPE' },
+  { src: '/assets/dao/blue-helium.png', title: 'Helium' },
+  { src: '/assets/dao/blue-uxd.png', title: 'UXDProtocol' },
   { src: '/assets/dao/blue-monke.png', title: 'MonkeDAO' },
   { src: '/assets/dao/blue-ukraine.png', title: 'Ukraine.SOL' },
-  { src: '/assets/dao/blue-ledao.png', title: 'Le DAO' },
 ] as const;
 
 interface Props {
